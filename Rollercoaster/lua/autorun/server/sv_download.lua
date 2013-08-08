e2265992135f8999a805e68e08eed2921182bc58
Foohy <swkauker@yahoo.com>
2013-08-07 17:04:30 -0700

    forgot some materials WOOPS

--- Rollercoaster/lua/autorun/server/sv_download.lua
+++ Rollercoaster/lua/autorun/server/sv_download.lua
@@ -1,21 +1,14 @@
 local convar = CreateConVar("coaster_downloadtoclients", 1, { FCVAR_ARCHIVE }, "Tell clients to download rollercoaster-related models and materials (requires changelevel to take effect)")
 
 if convar && convar:GetBool() then
+	//SOUNDS
 	resource.AddFile("sound/coaster_ride.wav")
 	resource.AddFile("sound/coaster_chain.wav")
 	resource.AddFile("sound/coaster_offdarailz.wav")
 	resource.AddFile("sound/coaster_wind.wav")
 	resource.AddFile("sound/coaster_sonic_the_carthog.mp3")
 
-	resource.AddFile("materials/sunabouzu/old_chain.vmt")
-	resource.AddFile("materials/sunabouzu/coaster_track.vmt")
-	resource.AddFile("materials/coaster/cart.vmt")
-	resource.AddFile("materials/coaster/remover.vmt")
-	resource.AddFile("materials/coaster/settings.vmt")
-	resource.AddFile("materials/coaster/track.vmt")
-	resource.AddFile("materials/coaster/save.vmt")
-
-
+	//THANKS SUNABOUZU
 	resource.AddFile("models/sunabouzu/coaster_base.mdl")
 	resource.AddFile("materials/models/sunabouzu/coaster_base.vmt")
 	resource.AddFile("materials/models/sunabouzu/coaster_base2.vmt")
@@ -27,5 +20,24 @@ if convar && convar:GetBool() then
 
 	resource.AddFile("materials/models/sunabouzu/sonic_the_carthog.vmt")
 	resource.AddFile("models/sunabouzu/sonic_the_carthog.mdl")
-	
+
+	//MORE STUNABOUZU
+	resource.AddFile("materials/sunabouzu/coaster_track.vmt")
+	resource.AddFile("materials/sunabouzu/old_chain.vmt")
+
+	//LOOK I'M LIKE SUNABOUZU
+	resource.AddFile("materials/foohy/warning.vmt")
+	resource.AddFile("materials/foohy/wood.vmt")
+
+	//COASTER STUFF
+	resource.AddFile("materials/coaster/cart.vmt")
+	resource.AddFile("materials/coaster/remover.vmt")
+	resource.AddFile("materials/coaster/save.vmt")
+	resource.AddFile("materials/coaster/settings.vmt")
+	resource.AddFile("materials/coaster/track.vmt")
+	resource.AddFile("materials/coaster/track_metal.vmt")
+	resource.AddFile("materials/coaster/track_metal_clean.vmt")
+	resource.AddFile("materials/coaster/track_wooden_metalrails.vmt")
+	resource.AddFile("materials/coaster/track_wooden_woodbeams.vmt")
+	resource.AddFile("materials/coaster/wood.vmt")	
 end
